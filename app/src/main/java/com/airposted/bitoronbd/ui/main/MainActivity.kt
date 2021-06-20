f60bbfcac3ebf7e9086ba060a60a72c738960e10
ashfaqurrahman <Ash@114431>
2021-06-20 12:20:45 +0600

    design update in receiver page

--- app/src/main/java/com/airposted/bitoronbd/ui/main/MainActivity.kt
+++ app/src/main/java/com/airposted/bitoronbd/ui/main/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import com.airposted.bitoronbd.R
 import com.airposted.bitoronbd.databinding.ActivityMainBinding
 import com.airposted.bitoronbd.ui.home.HomeFragment
+import com.airposted.bitoronbd.ui.product.ReceiverAddressFragment
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity(), KodeinAware, CommunicatorFragmentInter
         super.onCreate(savedInstanceState)
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        addContentFragment(HomeFragment(), false)
+        addContentFragment(ReceiverAddressFragment(), false)
 
     }
 
